d96bac79e67fe34e81bb5f882e6e3ae27b91dc5e
Thomas Weichselbaumer <thomas.weichselbaumer@netzberufler.de>
2023-01-12 15:43:59 +0100

    remove open and collapsed icon state from editor view

--- build/accordion/index.asset.php
+++ build/accordion/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '677944c1162b4bcdcd8f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '3e73e522d683296537e3');
